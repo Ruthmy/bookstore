fb128c544eeb16ac43894a820512e1e99cd7ddb6
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-15 08:57:11 -0600

    Modified 'DisplayBooks.js' -component- imported the Item component, and traversed the sample book array -BookList- to create the items in the interface.

--- src/components/DisplayBooks.js
+++ src/components/DisplayBooks.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Item from './Item';
+import BooksList from '../bd/BooksList';
 
 const DisplayBooks = () => (
   <div className="DisplayBooks">
     <h3>Home</h3>
+    {BooksList.map((book) => (
+      <Item key={book.item_id} item={book} />
+    ))}
+
   </div>
 );
 
