cbe431bd673a375cb4a8cedd3cb66f53e98c2e70
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-15 11:43:41 -0600

    Modified 'DisplayBooks.js' -component- Added the AddForm component and updated the imports.

--- src/components/DisplayBooks.js
+++ src/components/DisplayBooks.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Item from './Item';
 import BooksList from '../bd/BooksList';
+import '../styles/DisplayBooks.css';
+import AddForm from './AddForm';
 
 const DisplayBooks = () => (
   <div className="DisplayBooks">
-    <h3>Home</h3>
     {BooksList.map((book) => (
       <Item key={book.item_id} item={book} />
     ))}
-
+    <AddForm />
   </div>
 );
 
