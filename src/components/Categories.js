bdafd67db7f5806b2cb6767bbe464c03097f26ed
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-15 07:37:00 -0600

    Create 'Categories.js' -component- with a boiler plate describing a section under construction.

---
+++ src/components/Categories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Categories = () => (
+  <div className="categories">
+    <h3>Categories</h3>
+    <p>This section of our site is under construction.</p>
+  </div>
+);
+
+export default Categories;
