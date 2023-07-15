2b070368328ad97a07555f7194cb5b739fd0d52f
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-15 14:26:47 -0600

    Modified 'index.js' - component- Updated the import and the call for the main component 'App'.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App';
 import './index.css';
-import Bookstore from './components/Bookstore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Bookstore />
+    <App />
   </React.StrictMode>,
 );
