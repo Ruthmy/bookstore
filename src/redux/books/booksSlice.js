800f96a7ef339c452477f885f0d942e16ba361d7
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-20 09:02:23 -0600

    Modified 'booksSlice.js' -Slice State- Edited the initalState and improved the reducer to eliminate book.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,25 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [
-  {
-    item_id: 'item1',
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item2',
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item3',
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
-  },
-];
+const initialState = {
+  books: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
+};
 
 export const booksSlice = createSlice({
   name: 'books',
@@ -29,7 +31,10 @@ export const booksSlice = createSlice({
       state.books = [...state.books, action.payload];
     },
     deleteBook: (state, action) => {
-      state.filter((book) => book.id !== action.id);
+      const index = state.books.findIndex(
+        (book) => book.item_id === action.payload,
+      );
+      state.books.splice(index, 1);
     },
   },
 });
