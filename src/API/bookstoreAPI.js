eecb6220307741f3a37ece12e799424aa50de4a6
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-20 23:05:38 -0600

    Create 'bookstoreAPI.js' -file- To store the base URL and the ID from the API.

---
+++ src/API/bookstoreAPI.js
@@ -0,0 +1,6 @@
+export const bookstoreAPI = {
+  baseUrl: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/',
+  appId: '5ihhHAs4DSnZKuRj3eQY',
+};
+
+export default bookstoreAPI;
